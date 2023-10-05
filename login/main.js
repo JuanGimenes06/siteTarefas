08af3c86880830b3c69b0cfa8739ad39800325e6
Juan <joaopbg06@gmail.com>
2023-10-05 15:56:28 -0300

    Final commit

--- login/main.js
+++ login/main.js
@@ -6,19 +6,17 @@ form.addEventListener('submit', (event) => {
     // Adicionar o event.preventDefault() evita que a página seja recarregada quando o botão for apertado
     event.preventDefault();
 
-    // Obtém os valores dos campos de entrada do usuário e senha
-
     const usuario = usuarioInput.value.trim();
     const senha = senhaInput.value.trim();
 
     if(usuario === '' || senha === '') {
         alert("Por favor preencha os campos");
-    } else if (usuario === 'usuario' && senha === 'senha') {
-        // Verifica se as informações de login e senha estão corretas
+    } else if(Nusuario === '' || Nsenha === ''){
+        alert("Cadastre")
+    } else if (usuario === Nusuario && senha === Nsenha) {
         alert("Bem vindo!")
         location.replace("/../tarefa/index.html", "_self")
     } else {
-        // Exibe uma mensagem de erro se as informações estiverem, erradas
         alert( "Usuário ou senha incorreto, tente novamente" )
     
     }
