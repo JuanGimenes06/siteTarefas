98e51da79f92d57a985187694f738feb4ce0a3db
Juan <joaopbg06@gmail.com>
2023-10-05 16:54:55 -0300

    A

--- cadastro/main.js
+++ cadastro/main.js
@@ -1,7 +1,9 @@
 const forms = document.querySelector('.form');
+
 const inputUsuario = forms.querySelector('#usuario');
 const inputEmail = forms.querySelector('#email');
 const inputTelefone = forms.querySelector('#telefone');
+
 const inputSenha = forms.querySelector('#senha');
 const inputCsenha = forms.querySelector('#Csenha');
 
