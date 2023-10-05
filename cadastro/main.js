be392017b0c92f6a512785ac98ffa5571457414b
Juan <joaopbg06@gmail.com>
2023-10-05 15:59:43 -0300

    telefone and email

--- cadastro/main.js
+++ cadastro/main.js
@@ -1,5 +1,7 @@
 const forms = document.querySelector('.form');
 const inputUsuario = forms.querySelector('#usuario');
+const inputEmail = forms.querySelector('#email');
+const inputTelefone = forms.querySelector('#telefone');
 const inputSenha = forms.querySelector('#senha');
 const inputCsenha = forms.querySelector('#Csenha');
 
